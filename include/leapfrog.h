4bedef6ff5949471494f012628a1633c4559ed2c
Zhuoyue Zhao <zyzhao@cs.utah.edu>
2017-05-03 16:02:27 -0600

    to debug

--- include/leapfrog.h
+++ include/leapfrog.h
@@ -10,14 +10,15 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <memory>
 using std::uint8_t;
 
 typedef std::uint8_t lf_key_size_type;
 
 struct lf_key_comparator {
     bool operator()(const value_type &l, const value_type &r) const noexcept {
-        return (l.subject < r.subject) ||
-            (l.subject == r.subject && l.object < r.object);
+        return (l.key1 < r.key1) ||
+            (l.key1 == r.key1 && l.key2 < r.key2);
     }
 };
 
@@ -28,95 +29,112 @@ struct lf_key_info {
 
 template<typename ...T>
 struct lf_join {
-    typedef tpie::btree<value_type, btree_comp<lf_key_comparator>, T...> btree_type;
-
-    std::vector<btree_type> m_btrees;
-    std::vector<lf_key_info> m_keyinfo;
-    std::vector<std::vector<lf_iter_info>> m_iterinfo;
-    uint64_t m_count;
-    std::vector<uint64_t> m_pos;
+    typedef tpie::btree<value_type, tpie::btree_comp<lf_key_comparator>, T...> btree_type;
     
     struct lf_iter_ref {
-        btree_type::iterator m_iter;
+        typename btree_type::iterator m_iter;
     };
 
     struct lf_iter_info {
         lf_key_size_type m_table_id,
                          m_key_id;
-        lf_iter_ref &m_iter_ref;
-        btree_type &m_btree;
+        std::shared_ptr<lf_iter_ref> m_iter_ref;
+        btree_type *m_btree;
         lf_iter_info *m_next_iter_info, *m_prev_iter_info;
-        bool m_at_first;
-
-        lf_iterinfo(lf_key_size_type table_id,
-                    lf_key_size_type key_id,
-                    lf_iter_ref &iter_ref,
-                    btree_type &btree)
-            : m_table_id(table_id), m_key_id(key_id), m_iter_ref(&iter_ref),
-              m_btree(btree),
-              m_base_value{iter_ref.m_iter->subject, iter_ref.m_iter->object},
-              m_next_iter_info(nullptr), m_prev_iter_info(nullptr),
-              m_at_first(true) {}
-        
         value_type m_base_value;
+        
+        lf_iter_info(lf_key_size_type table_id, 
+                lf_key_size_type key_id,
+                typename btree_type::iterator iter,
+                btree_type *btree):
+            lf_iter_info(table_id, key_id, std::shared_ptr<lf_iter_ref>(new lf_iter_ref{iter}), btree) {
+            
+        }
 
-        int key() {
-            return static_cast<attr_type *>(&*(m_iter_ref.m_iter))[m_key_id];
+        lf_iter_info(lf_key_size_type table_id,
+                    lf_key_size_type key_id,
+                    std::shared_ptr<lf_iter_ref> iter_ref,
+                    btree_type *btree)
+            : m_table_id(table_id), m_key_id(key_id),
+              m_iter_ref(iter_ref), m_btree(btree),
+              m_base_value{iter_ref->m_iter->key1, iter_ref->m_iter->key2},
+              m_next_iter_info(nullptr), m_prev_iter_info(nullptr) {}
+
+
+        int key() const noexcept {
+            return reinterpret_cast<const attr_type *>(&*(m_iter_ref->m_iter))[m_key_id];
         }
 
         void next() {
-            if (m_at_first) {
-                m_at_first = false;
-            } else {
-                ++m_iter_ref.m_iter;
-            }
+            seek(key() + 1);
         }
 
-        bool atEnd() {
+        bool atEnd() const noexcept {
             for (lf_key_size_type i = 0; i <= m_key_id; ++i) {
-                if (static_cast<attr_type *>(&*(m_iter_ref.m_iter))[i] != 
-                    static_cast<attr_type *>(&m_base_value)[i]) return true;
+                if (reinterpret_cast<const attr_type *>(&*(m_iter_ref->m_iter))[i] != 
+                    reinterpret_cast<const attr_type *>(&m_base_value)[i]) return true;
             }
             return false;
         }
 
         void seek(attr_type key) {
-            static_cast<attr_type *>(&m_base_value)[m_key_id] = key;
+            reinterpret_cast<attr_type *>(&m_base_value)[m_key_id] = key;
             for (lf_key_size_type i = m_key_id + 1; i < 2; ++i) {
-                static_cast<attr_type *>(&m_base_value)[i] = 0;
+                reinterpret_cast<attr_type *>(&m_base_value)[i] = 0;
             }
-            m_iter_ref.m_iter = m_btree.lower_bound(m_base_value);
-            m_at_first = false;
+            m_iter_ref->m_iter = m_btree->lower_bound(m_base_value);
         }
 
         void open() {
             if (m_next_iter_info) {
-                m_next_iter_info->m_base_value = *(m_iter_ref.m_iter);
-                m_next_iter_info->m_at_first = true;
+                m_next_iter_info->m_base_value = *(m_iter_ref->m_iter);
             }
         }
 
         void up() {
+            /*if (m_prev_iter_info) {
+                m_prev_iter_info->seek(
+                    reinterpret_cast<attr_type *>(&m_prev_iter_info->m_base_value)[m_key_id - 1] + 1);
+                m_prev_iter_info->m_at_first = true;
+            } */
             if (m_prev_iter_info) {
-                m_next_iter_info->seek(m_next_iter_info->m_base_value[m_key_id - 1] + 1);
-                m_next_iter_info->m_at_first = true;
+                m_prev_iter_info->seek(
+                    reinterpret_cast<attr_type *>(&m_base_value)[m_key_id - 1]);
             }
         }
     };
+
+    static constexpr bool is_internal =
+        tpie::bbits::tree_state<value_type, 
+            typename tpie::bbits::OptComp<T...>::type>::is_internal;
+
+    std::vector<btree_type> m_btrees;
+    std::vector<lf_key_info> m_keyinfo;
+    std::vector<std::vector<lf_iter_info*>> m_iterinfo;
+    uint64_t m_count;
+    std::vector<uint64_t> m_pos;
 
     auto nrels() { return m_btrees.size(); }
 
     lf_join() {}
 
-    ~lf_join() {}
+    ~lf_join() {
+        for (auto &v: m_iterinfo) {
+            for (auto iterinfo: v) {
+                delete iterinfo;
+            }
+        }
+    }
     
     /* assuming that the file is sorted */
+    template <typename X=tpie::bbits::enab>
     void load_internal_table(tpie::file_stream<value_type> &in,
                     lf_key_size_type subject_depth,
-                    lf_key_size_type object_depth) {
+                    lf_key_size_type object_depth,
+                    tpie::bbits::enable<X, is_internal> = tpie::bbits::enab()) {
         
-        tpie::btree_builder<value_type, btree_comp<lf_key_comparator>, 
-            tpie::btree_internal, T...> builder;
+        tpie::btree_builder<value_type, tpie::btree_comp<lf_key_comparator>,
+            T...> builder;
         value_type obj;
         while (in.can_read()) {
             obj = in.read();
@@ -126,12 +144,14 @@ struct lf_join {
         m_keyinfo.emplace_back(lf_key_info{subject_depth, object_depth});
     }
     
+    template <typename X=tpie::bbits::enab>
     void load_into_external_table(tpie::file_stream<value_type> &in,
             lf_key_size_type subject_depth,
             lf_key_size_type object_depth,
-            std::string path) {
-        tpie::btree_builder(value_type, btree_comp<lf_key_comparator>,
-                tpie::btree_external, T...> builder(path);
+            std::string path,
+            tpie::bbits::enable<X, !is_internal> = tpie::bbits::enab()) {
+        tpie::btree_builder<value_type, tpie::btree_comp<lf_key_comparator>,
+                T...> builder(path);
         value_type obj;
         while (in.can_read()) {
             obj = in.read();
@@ -146,32 +166,30 @@ private:
         m_iterinfo.clear();
         m_iterinfo.resize(1);
         for (lf_key_size_type table_id = 0; table_id < m_keyinfo.size(); ++table_id) {
-            m_iterinfo[0].emplace_back(table_id, ~0U, m_btrees[table_id].begin());
-            const lf_key_size_type *a_keyinfo = (const lf_key_size_type *) &keyinfo[table_id];
+            m_iterinfo[0].emplace_back(new lf_iter_info(table_id, (lf_key_size_type) ~0U,
+                    m_btrees[table_id].begin(), &m_btrees[table_id]));
+            const lf_key_size_type *a_keyinfo = (const lf_key_size_type *) &m_keyinfo[table_id];
             lf_iter_info *prev_iter_info = nullptr;
             for (lf_key_size_type i = 0; i < 2; ++i) {
                 if (a_keyinfo[i] >= m_iterinfo.size()) {
                     m_iterinfo.resize(a_keyinfo[i] + 1);
                 }
-                m_iterinfo[a_keyinfo[i]].emplace_back(table_id, i,
-                        m_iterinfo[0][table_id].iter);
-                m_iterinfo[a_keyinfo[i]].back().m_prev_iter_info = prev_iter_info;
-                prev_iter_info->m_next_iter_info = &(m_iterinfo[a_keyinfo[i]].back());
-                prev_iter_info = &(m_iterinfo[a_keyinfo[i]].back());
-                //if (i == 1) {
-                //    m_iterinfo[a_keyinfo[i]].back().m_next_iter_info = 
-                //        &(m_iterinfo[0].back());
-                //}
+                m_iterinfo[a_keyinfo[i]].emplace_back(new lf_iter_info(table_id, i,
+                        m_iterinfo[0][table_id]->m_iter_ref, &m_btrees[table_id]));
+                m_iterinfo[a_keyinfo[i]].back()->m_prev_iter_info = prev_iter_info;
+                if (prev_iter_info)
+                    prev_iter_info->m_next_iter_info = m_iterinfo[a_keyinfo[i]].back();
+                prev_iter_info = m_iterinfo[a_keyinfo[i]].back();
             }
         }
         
-        std::cerr << "total depth = " << m_iterinfo.size() - 1; << std::endl;
+        std::cerr << "total depth = " << m_iterinfo.size() - 1 << std::endl;
         for (lf_key_size_type depth = 0; depth < m_iterinfo.size(); ++depth) {
-            std::cerr << "depth " << depth << ':' ;
+            std::cerr << "depth " << (unsigned) depth << ':';
             if (m_iterinfo[depth].empty()) return true;
             for (const auto &iterinfo: m_iterinfo[depth]) {
-                std::cerr << " {" << iterinfo.table_id << ", "
-                    << iterinfo.key_id << "}";
+                std::cerr << " {" << (unsigned) iterinfo->m_table_id << ", "
+                    << (unsigned) iterinfo->m_key_id << "}";
             }
             std::cerr << std::endl;
         }
@@ -181,14 +199,14 @@ private:
     /* @returns atEnd */
     void init(lf_key_size_type depth) {
         for (lf_key_size_type i = 0; i < m_iterinfo[depth].size(); ++i) {
-            if (m_iterinfo[depth][i].atEnd()) {
+            if (m_iterinfo[depth][i]->atEnd()) {
                 m_pos.push_back(i);
                 return ;
             }
         }
-        std::sort(m_iterinfo[i].begin(), m_iterinfo[i].end(),
-                [&](const lf_iter_info &l, const lf_iter_info &r) -> bool {
-                    return l.key() < r.key();
+        std::sort(m_iterinfo[depth].begin(), m_iterinfo[depth].end(),
+                [&](const lf_iter_info *l, const lf_iter_info *r) -> bool {
+                    return l->key() < r->key();
                 }
         );
         m_pos.push_back(0ull);
@@ -196,19 +214,19 @@ private:
     }
 
     void search(lf_key_size_type depth) {
-        auto k = m_iterinfo.size();
+        auto k = m_iterinfo[depth].size();
         auto p = m_pos.back();
-        attr_type max_key = m_iterinfo[depth][(p + k - 1) % k].key();
+        attr_type max_key = m_iterinfo[depth][(p + k - 1) % k]->key();
         for (;;) {
-            auto key = m_iterinfo[depth][p].key();
+            auto key = m_iterinfo[depth][p]->key();
             if (key == max_key) {
                 break;
             } else {
-                m_iterinfo[depth][p].seek(max_key);
-                if (m_iterinfo[depth][p].atEnd()) {
+                m_iterinfo[depth][p]->seek(max_key);
+                if (m_iterinfo[depth][p]->atEnd()) {
                     break;
                 } else {
-                    max_key = m_iterinfo[depth][p].key();
+                    max_key = m_iterinfo[depth][p]->key();
                     p = (p + 1) % k;
                 }
             }
@@ -217,11 +235,12 @@ private:
     }
 
     void next(lf_key_size_type depth) {
+        auto k = m_iterinfo[depth].size();
         auto p = m_pos.back();
-        m_iterinfo[depth][p].next();
-        if (!m_iterinfo[depth][p].atEnd()) {
-            p = (p + 1) % k;
-            search();
+        m_iterinfo[depth][p]->next();
+        if (!m_iterinfo[depth][p]->atEnd()) {
+            m_pos.back() = (p + 1) % k;
+            search(depth);
         }
     }
 
@@ -229,25 +248,29 @@ private:
         m_pos.clear();
         lf_key_size_type depth = 1;
         while (depth != 0) {
-            if (m_pos.length != depth) {
+            if (m_pos.size() != depth) {
                 init(depth);
             } else {
                 next(depth);
             }
             auto p = m_pos.back();
-            if (m_iterinfo[depth][p].atEnd()) {
-                m_pos.pop();
-                for (auto &iter_info: m_iterinfo[depth]) {
-                    iter_info.up();
+            if (m_iterinfo[depth][p]->atEnd()) {
+                m_pos.pop_back();
+                for (auto iter_info: m_iterinfo[depth]) {
+                    iter_info->up();
                 }
                 --depth;
             } else {
                 if (depth + 1 == m_iterinfo.size()) {
+                    for (lf_key_size_type i = 1; i < m_iterinfo.size(); ++i) {
+                        std::cout << m_iterinfo[i][0]->key() << ' ';
+                    }
+                    std::cout << std::endl;
                     ++m_count;
                 } else {
                     ++depth;
-                    for (auto &iter_info: m_iterinfo[depth]) {
-                        iter_info.open();
+                    for (auto iter_info: m_iterinfo[depth]) {
+                        iter_info->open();
                     }
                 }
             }
--- src/leapfrog.cpp
+++ src/leapfrog.cpp
@@ -3,4 +3,321 @@
 #include <tpie/memory.h>
 #include <tpie/btree.h>
 #include <tpie/file_stream.h>
+#include <tpie/sort.h>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <utility>
+#include <unistd.h>
+#include <tuple>
+#include <fstream>
+#include <cstdio>
 using namespace std;
+
+typedef tuple<attr_type, attr_type, attr_type> triple_t;
+
+struct dictionary_t {
+    vector<string> mapping;
+    unordered_map<string, attr_type> inverted_index;
+
+    dictionary_t() {}
+    dictionary_t(dictionary_t &&dict):
+        mapping(move(dict.mapping)),
+        inverted_index(move(dict.inverted_index)) {}
+    dictionary_t(const dictionary_t &dict):
+        mapping(dict.mapping),
+        inverted_index(dict.inverted_index) {}
+
+    dictionary_t &operator=(dictionary_t &&dict) {
+        mapping = move(dict.mapping);
+        inverted_index = move(dict.inverted_index);
+        return *this;
+    }
+    dictionary_t &operator=(const dictionary_t &dict) {
+        mapping = dict.mapping;
+        inverted_index = dict.inverted_index;
+        return *this;
+    }
+
+    dictionary_t &add(const string &s) {
+        if (inverted_index.find(s) == inverted_index.end()) {
+            mapping.push_back(s);
+            inverted_index.emplace(s, mapping.size() - 1);
+        }
+        return *this;
+    }
+
+    void rebuild_inverted_index() {
+        inverted_index.clear();
+        for (vector<string>::size_type i = 0; i != mapping.size(); ++i) {
+            inverted_index.emplace(mapping[i], i);
+        }
+    }
+
+    attr_type lookup(const string &s) const {
+        return inverted_index.at(s);
+    }
+};
+
+bool parse_turtle(const string &line, tuple<string, string, string> &tuple) {
+    auto p1 = line.find('<'); 
+    if (p1 == string::npos) return false;
+    auto p2 = line.find('>', p1 + 1);
+    if (p2 == string::npos) return false;
+    auto p3 = line.find('<', p2 + 1);
+    if (p3 == string::npos) return false;
+    auto p4 = line.find('>', p3 + 1);
+    if (p4 == string::npos) return false;
+    auto p5 = line.find_first_of("<\"", p4 + 1);
+    if (p5 == string::npos) return false;
+    auto p6 = (line[p5] == '<') ? line.find('>', p5 + 1) :
+        line.find('"', p5 + 1);
+    if (p6 == string::npos) return false;
+    auto p7 = line.find('.', p6 + 1);
+    if (p7 == string::npos || p7 + 1 != line.length()) return false;
+    tuple = make_tuple(line.substr(p1, p2 - p1 + 1),
+            line.substr(p3, p4 - p3 + 1),
+            line.substr(p5, p6 - p5 + 1));
+    return true;
+}
+
+bool load_dictionary(string data_dir, dictionary_t &out_dict) {
+    dictionary_t dict;
+    
+    ifstream dict_file(data_dir + "/dictionary.txt");
+    if (!dict_file.good()) return false;
+    string line;
+    getline(dict_file, line);
+    auto dict_size = stoull(line);
+    dict.mapping.reserve(dict_size);
+    while (getline(dict_file, line)) {
+        dict.mapping.emplace_back(move(line));
+    }
+    dict.rebuild_inverted_index();
+
+    out_dict = move(dict);
+    return true;
+}
+
+bool create_dictionary(string data_dir, dictionary_t &out_dict) {
+    dictionary_t dict;
+    
+    ifstream file_list(data_dir + "/file_list.txt");
+    if (!file_list.good()) return false;
+    string file_name;
+    string turtle;
+    tuple<string, string, string> spo;
+    while (getline(file_list, file_name), !file_name.empty()) {
+        ifstream ttl_file(data_dir + "/" + file_name);
+        if (!ttl_file.good()) return false;
+        while (getline(ttl_file, turtle)) {
+            decltype(turtle.length()) p = 0;
+            while (p < turtle.length() && turtle[p] == ' ') ++p;
+            if (p == turtle.length() || turtle[p] == '#') continue;
+            if (!parse_turtle(turtle, spo)) return false;
+            dict.add(get<0>(spo)).add(get<1>(spo)).add(get<2>(spo));        
+        }
+    }
+
+    ofstream dict_file(data_dir + "/dictionary.txt");
+    dict_file << dict.mapping.size() << endl;
+    for (const auto &s: dict.mapping) {
+        dict_file << s << endl;
+    }
+
+    out_dict = move(dict);
+    return true;
+}
+
+bool load_or_create_dictionary(string data_dir, dictionary_t &out_dict) {
+    if (access((data_dir + "/dictionary.txt").c_str(), F_OK)) {
+        return create_dictionary(data_dir, out_dict);
+    } else {
+        return load_dictionary(data_dir, out_dict);
+    }
+}
+
+bool read_and_sort_by_predicate(string data_dir, const dictionary_t &dict) {
+    ifstream file_list(data_dir + "/file_list.txt");
+    if (!file_list.good()) return false;
+    
+    cerr << "concatenating files ..." << endl;
+    tpie::file_stream<triple_t> out;
+    out.open(data_dir + "/sorted_by_predicate.dat");
+
+    string ttl_file_name;
+    string turtle;
+    tuple<string, string, string> spo;
+    triple_t triple;
+    while (getline(file_list, ttl_file_name), !ttl_file_name.empty()) {
+        ifstream ttl_file(data_dir + "/" + ttl_file_name);
+        if (!ttl_file.good()) return false;
+        while (getline(ttl_file, turtle)) {
+            decltype(turtle.length()) p = 0;
+            while (p < turtle.length() && turtle[p] == ' ') ++p;
+            if (p == turtle.length() || turtle[p] == '#') continue;
+            if (!parse_turtle(turtle, spo)) return false;
+            triple = make_tuple(dict.lookup(get<1>(spo)),
+                        dict.lookup(get<0>(spo)),
+                        dict.lookup(get<2>(spo)));
+            out.write(triple);
+        }
+    }
+    
+    cerr << "sorting ..." << endl;
+    tpie::sort(out, out);
+
+    return true;
+}
+
+bool create_partitioned_tables(string data_dir, const dictionary_t &dict) {
+    cerr << "creating partitioned tables ..." << endl;
+    tpie::file_stream<triple_t> in;
+    tpie::file_stream<pair<attr_type, attr_type>> out, outr;
+    in.open(data_dir + "/sorted_by_predicate.dat", tpie::access_read);
+    vector<attr_type> predicates;
+    
+    triple_t triple;
+    if (!in.can_read()) {
+        return false;
+    }
+    triple = in.read();
+    predicates.push_back(get<0>(triple));
+    out.open(data_dir + "/" + to_string(predicates.back()) + ".dat", tpie::access_write);
+    out.write(make_pair(get<1>(triple), get<2>(triple)));
+    outr.open(data_dir + "/" + to_string(predicates.back()) + "r.dat", tpie::access_write);
+    outr.write(make_pair(get<2>(triple), get<1>(triple)));
+    while (in.can_read()) {
+        triple = in.read();
+        attr_type predicate = get<0>(triple);
+        if (predicate != predicates.back()) {
+            out.close();
+            tpie::sort(outr, outr);
+            outr.close();
+            predicates.push_back(predicate);
+            out.open(data_dir + "/" + to_string(predicates.back()) + ".dat", tpie::access_write);
+            outr.open(data_dir + "/" + to_string(predicates.back()) + "r.dat", tpie::access_write);
+        }
+        out.write(make_pair(get<1>(triple), get<2>(triple)));
+        outr.write(make_pair(get<2>(triple), get<1>(triple)));
+    }
+    out.close();
+    tpie::sort(outr, outr);
+    outr.close();
+
+    ofstream predicate_list(data_dir  + "/predicate_list.txt");
+    predicate_list << predicates.size() << endl;
+    for (auto predicate: predicates) {
+        predicate_list << predicate << ' ' << dict.mapping[predicate] << endl;
+    }
+    
+    return true;
+}
+
+bool check_or_transform_turtle(string data_dir, const dictionary_t &dict) {
+    if (access((data_dir + "/predicate_list.txt").c_str(), F_OK)) {
+        if (access((data_dir + "/sorted_by_predicate.dat").c_str(), F_OK)) {
+            if (!read_and_sort_by_predicate(data_dir, dict)) {
+                return false;
+            }
+        }
+        if (!create_partitioned_tables(data_dir, dict)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void remove_files(string data_dir) {
+    remove((data_dir + "/dictionary.txt").c_str());
+    remove((data_dir + "/sorted_by_predicate.dat").c_str());
+    ifstream predicate_list(data_dir + "/predicate_list.txt");
+    string line;
+    getline(predicate_list, line);
+    while (getline(predicate_list, line), !line.empty()) {
+        auto p = line.find(' ');
+        remove((data_dir + "/" + line.substr(0, p) + ".dat").c_str());
+        remove((data_dir + "/" + line.substr(0, p) + "r.dat").c_str());
+    }
+    predicate_list.close();
+    remove((data_dir + "/predicate_list.txt").c_str());
+}
+
+void usage(char *progname) {
+    cout  << "usage: " << progname << " [-f] <data_dir> <mem_limit (GB)>" << endl;
+}
+
+void run_query(string data_dir, const dictionary_t &dict) {
+    ifstream query(data_dir + "/query.txt");
+    if (!query.good()) return ;
+    
+    lf_join<tpie::btree_internal> join;
+    string line;
+    while (getline(query, line), !line.empty()) {
+        auto p = line.find(' ');
+        auto subject_depth = stoull(line.substr(0, p));
+        auto p2 = line.find(' ', p + 1);
+        auto object_depth = stoull(line.substr(p + 1, p2 - p - 1));
+        auto predicate_str = line.substr(p2 + 1);
+        auto predicate = dict.lookup(predicate_str);
+
+        tpie::file_stream<value_type> in;
+        if (subject_depth < object_depth) {
+            in.open(data_dir + "/" + to_string(predicate) + ".dat", tpie::access_read);
+            join.load_internal_table(in, subject_depth, object_depth);
+        } else {
+            in.open(data_dir + "/" + to_string(predicate) + "r.dat", tpie::access_read);
+            join.load_internal_table(in, object_depth, subject_depth);
+        }
+    }
+
+    auto count = join.join_count();
+    cout << "count = " << count << endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        usage(argv[0]);
+        return 1;
+    }
+    
+    int argi = 1;
+    bool force_rebuild = false;
+    if (!strcmp(argv[argi], "-f")) {
+        ++argi;
+        force_rebuild = true;
+    }
+    const string data_dir = argv[argi++];
+    if (argi == argc) {
+        usage(argv[0]);
+        return 1;
+    }
+    const unsigned long long mem_limit = stoull(argv[argi++]) * 1024 * 1024 * 1024;
+
+    tpie::tpie_init();
+    tpie::get_memory_manager().set_limit(mem_limit);
+
+    if (force_rebuild) {
+        remove_files(data_dir);
+    }
+
+    dictionary_t dict;
+    if (!load_or_create_dictionary(data_dir, dict)) {
+        cout << "[ERROR] load dictionary" << endl;
+        return 1;
+    }
+
+    if (!check_or_transform_turtle(data_dir, dict)) {
+        cout << "[ERROR] transform turtle" << endl;
+        return 1;
+    }
+
+    run_query(data_dir, dict);
+
+    tpie::tpie_finish();
+    cout << "<DONE>" << endl;
+
+    return 0;
+
+}
